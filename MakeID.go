eac18bee7883342dbfb68d8b87c604295159e334
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-11-09 09:32:19 +1100

    add makeid with custom charset

--- MakeID.go
+++ MakeID.go
@@ -31,3 +31,23 @@ func MakeID(n int) string {
 
 	return string(b)
 }
+
+//MakeIDCustomCharSet randomized string id with custom charSet
+func MakeIDCustomCharSet(n int, charSet string) string {
+	rand.Seed(time.Now().UTC().UnixNano() - rand.Int63())
+	b := make([]byte, n)
+	// A rand.Int63() generates 63 random bits, enough for letterIdxMax letters!
+	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = rand.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(charSet) {
+			b[i] = charSet[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
